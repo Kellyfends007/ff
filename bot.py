30218f5866f0e167348771ad0a6699b3ac5430a8
Kellyfends007 <54773252+Kellyfends007@users.noreply.github.com>
2019-09-02 17:40:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,25 +4,7 @@ bot = telebot.TeleBot('819903094:AAG_OWLboTUD7qw2dzjgWlYg3r1uTBLspJw')
 keyboard1 = telebot.types.ReplyKeyboardMarkup()
 keyboard1.row('Привет', 'Пока')
 
-if "HEROKU" in list(os.environ.keys()):
-    logger = telebot.logger
-    telebot.logger.setLevel(logging.INFO)
-    
-        @server.route("/bot", methods=['POST'])
-    def getMessage():
-        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-        return "!", 200
-    @server.route("/")
-    def webhook():
-        bot.remove_webhook()
-        bot.set_webhook(url="https://min-gallows.herokuapp.com/bot") # этот url нужно заменить на url вашего Хероку приложения
-        return "?", 200
-    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
-else:
-    # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.  
-    # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
-    bot.remove_webhook()
-    bot.polling(none_stop=True)
+
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
